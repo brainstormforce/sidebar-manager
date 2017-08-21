41b3e5e42e5343e68389545f0d74463cd272c762
Nikhil Chavan <email@nikhilchavan.com>
2017-08-21 14:15:55 +0530

    Initial Plugin scaffold

---
+++ custom-sidebars.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Custom Sidebars
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     custom-sidebars
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Custom_Sidebars
+ */
