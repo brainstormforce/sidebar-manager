84cef67a9e8fa832552bde1cef2c9e88cac7a39d
yssambare12 <bloggingscan@gmail.com>
2023-07-11 11:51:31 +0530

    Fix:Display ruleset not working as expected on singluar post/tag/page

--- classes/modules/target-rule/class-bsf-sb-target-rules-fields.php
+++ classes/modules/target-rule/class-bsf-sb-target-rules-fields.php
@@ -681,11 +681,32 @@ if ( ! class_exists( 'BSF_SB_Target_Rules_Fields' ) ) {
 
 						// taxonomy options.
 						if ( strpos( $sel_value, 'tax-' ) !== false ) {
-							$tax_id        = (int) str_replace( 'tax-', '', $sel_value );
-							$term          = get_term( $tax_id );
-							$term_taxonomy = ucfirst( str_replace( '_', ' ', $term->taxonomy ) );
-							$output       .= '<option value="tax-' . $tax_id . '" selected="selected" >' . $term->name . ' - ' . $term_taxonomy . '</option>';
-
+						
+						
+						$suffix = "";
+						
+						$term_suffix = "";
+						
+						if (preg_match("/\d+(-single-.+)$/", $sel_value, $m)) {
+						
+						$suffix = $m[1];
+						
+						$term_suffix = " (Singulars)";
+						
+						}
+						
+						
+						
+						$tax_id = (int) str_replace( 'tax-', '', $sel_value ); // THIS BIT HERE WOULD CHOP OFF TEXT AFTER THE TERM-ID
+						
+						$term = get_term( $tax_id );
+						
+						$term_taxonomy = ucfirst( str_replace( '_', ' ', $term->taxonomy ) );
+						
+						$output .= '<option value="tax-' . $tax_id . $suffix . '" selected="selected" >' . $term->name . ' - ' . $term_taxonomy . $term_suffix . '</option>';
+						
+						
+						
 						}
 					}
 				}
