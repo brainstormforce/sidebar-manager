bc232326a8e89068626c744c278df594d6bd385d
Yogesh Sambare <91533131+yssambare12@users.noreply.github.com>
2025-03-31 12:43:17 +0530

    chore:fix security issue from astra notices lib

--- sidebar-manager.php
+++ sidebar-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name:     Sidebar Manager
  * Plugin URI:      http://www.brainstormforce.com
  * Description:     This is the plugin to create custom siderbars to your site.
- * Version:         1.1.8
+ * Version:         1.1.9
  * Author:          Brainstorm Force
  * Author URI:      https://www.brainstormforce.com/
  * Text Domain:     bsfsidebars
@@ -25,7 +25,7 @@ define( 'BSF_SB_FILE', __FILE__ );
 define( 'BSF_SB_BASE', plugin_basename( BSF_SB_FILE ) );
 define( 'BSF_SB_DIR', plugin_dir_path( BSF_SB_FILE ) );
 define( 'BSF_SB_URL', plugins_url( '/', BSF_SB_FILE ) );
-define( 'BSF_SB_VER', '1.1.8' );
+define( 'BSF_SB_VER', '1.1.9' );
 define( 'BSF_SB_PREFIX', 'bsf-sb' );
 define( 'BSF_SB_POST_TYPE', 'bsf-sidebar' );
 
